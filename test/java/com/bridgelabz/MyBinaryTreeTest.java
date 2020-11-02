a1653463f6bfc189d1e0114e78719e78189f3a73
asmitamthorat <asmita26296@gmail.com>
2020-11-02 13:47:44 +0530

    ADDED:created binary tree provided in use-case

--- test/java/com/bridgelabz/MyBinaryTreeTest.java
+++ test/java/com/bridgelabz/MyBinaryTreeTest.java
@@ -18,5 +18,27 @@ public class MyBinaryTreeTest {
         Assert.assertEquals(6,size);
     }
 
+    @Test
+    public void givenNumber_WhenAddedToBinaryTree_ShouldReturnSizeThree(){
+        MyBinaryTree<Integer> myBinaryTree=new MyBinaryTree();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        myBinaryTree.add(22);
+        myBinaryTree.add(40);
+        myBinaryTree.add(11);
+        myBinaryTree.add(3);
+        myBinaryTree.add(16);
+        myBinaryTree.add(60);
+        myBinaryTree.add(95);
+        myBinaryTree.add(65);
+        myBinaryTree.add(63);
+        myBinaryTree.add(67);
+        int size =myBinaryTree.getSize();
+        Assert.assertEquals(13,size);
+    }
+
+
+
 
 }
